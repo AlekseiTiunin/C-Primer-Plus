c0a20f20092126903a000b82b7ac7860d3092e43
AlekseiTiunin <39889212+AlekseiTiunin@users.noreply.github.com>
2018-08-27 15:03:53 +0300

    Create 2_1.cpp

---
+++ Chapter 2/2_1.cpp
@@ -0,0 +1,14 @@
+// myname.cpp
+
+#include <iostream>
+
+int main(void)
+{
+    using std::cout;
+    using std::endl;
+    
+    cout << "My name Aleksei" << endl;
+    cout << "I'm from Saint-Petersburg" << endl;
+    
+    return 0;
+}
