888457310c7a3309a45b3c197ca09d24fbb98205
AlekseiTiunin <39889212+AlekseiTiunin@users.noreply.github.com>
2018-08-30 17:42:35 +0300

    Create 4_2.cpp

---
+++ Chapter 4/4_2.cpp
@@ -0,0 +1,25 @@
+// insrt2_string.cpp
+
+#include <iostream>
+#include <cstring>
+
+int main()
+{
+    using std::cout;
+    using std::cin;
+    using std::endl;
+    using std::string;
+    using std::getline;
+
+    string name;
+    string dessert;
+
+    cout << "Enter your name:\n";
+    getline(cin, name);
+    cout << "Enter your favorite dessert:\n";
+    getline(cin, dessert);
+    cout << "I have some delicious " << dessert;
+    cout << " for you, " << name << ".\n";
+
+    return 0;
+}
