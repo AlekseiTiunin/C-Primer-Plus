c806bb18cce541770ef539e8c9f9f3e98aeacbe2
AlekseiTiunin <39889212+AlekseiTiunin@users.noreply.github.com>
2018-11-03 11:56:55 +0300

    Create 10_04_sales.h

---
+++ Chapter 10/10_04_sales.h
@@ -0,0 +1,18 @@
+#pragma once
+namespace SALES
+{
+	class Sales
+	{
+	private:
+		static const int QUARTERS = 4;
+		double sales[QUARTERS];
+		double average;
+		double max;
+		double min;
+	public:
+		Sales();
+		Sales(const double  * ar, int n);
+		void set();
+		void show() const;
+	};
+}
