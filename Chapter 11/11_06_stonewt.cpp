3e9395a9f3778d2c76a44ad1b8f3f34aabb36d57
AlekseiTiunin <39889212+AlekseiTiunin@users.noreply.github.com>
2018-12-05 17:17:21 +0300

    Create 11_06_stonewt.cpp

---
+++ Chapter 11/11_06_stonewt.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+using std::cout;
+#include "stonewt.h"
+
+Stonewt::Stonewt(double lbs, Mode mode)
+{	
+	if (mode != INTLBS && mode != FLOATLBS && mode != STONE)
+	{
+		std::cout << "Mode must be INTLBS, FLOATLBS or STONE.\n";
+		std::cout << "Setting weight to 0.\n";
+		stone = 0;
+		pds_left = 0;
+		lbs = 0;
+		state = FLOATLBS;
+	}
+	else
+	{
+		stone = int(lbs) / Lbs_per_stn;
+		pds_left = int(lbs) % Lbs_per_stn + lbs - int(lbs);
+		pounds = lbs;
+		state = mode;
+	}
+}
+
+Stonewt::Stonewt(int lbs, Mode mode)
+{
+	if (mode != INTLBS && mode != FLOATLBS && mode != STONE)
+	{
+		std::cout << "Mode must be INTLBS, FLOATLBS or STONE.\n";
+		std::cout << "Setting weight to 0.\n";
+		stone = 0;
+		pds_left = 0;
+		lbs = 0;
+		state = FLOATLBS;
+	}
+	else
+	{
+		stone = int(lbs) / Lbs_per_stn;
+		pds_left = int(lbs) % Lbs_per_stn + lbs - int(lbs);
+		pounds = lbs;
+		state = mode;
+	}
+}
+
+
+Stonewt::Stonewt(int stn, double lbs, Mode mode)
+{
+	if (mode != INTLBS && mode != FLOATLBS && mode != STONE)
+	{
+		std::cout << "Mode must be INTLBS, FLOATLBS or STONE.\n";
+		std::cout << "Setting weight to 0.\n";
+		stone = 0;
+		pds_left = 0;
+		lbs = 0;
+		state = FLOATLBS;
+	}
+	else
+	{
+		stone = stn;
+		pds_left = lbs;
+		pounds = stn * Lbs_per_stn + lbs;
+		state = mode;
+	}
+}
+
+Stonewt::Stonewt()
+{
+	stone = 0;
+	pounds = pds_left = 0;
+	state = FLOATLBS;
+}
+
+Stonewt::~Stonewt()
+{
+}
+
+void Stonewt::SetMode(Mode mode)
+{
+	state = mode;
+}
+
+Stonewt Stonewt::operator+(const Stonewt & b) const
+{
+	return Stonewt(pounds + b.pounds);
+}
+
+Stonewt Stonewt::operator-(const Stonewt & b) const
+{
+	return Stonewt(pounds - b.pounds);
+}
+
+Stonewt Stonewt::operator*(double n) const
+{
+	return Stonewt(n * pounds);
+}
+
+Stonewt operator*(double n, const Stonewt &a)
+{
+	return a * n;
+}
+
+std::ostream & operator<<(std::ostream & os, const Stonewt & stn)
+{
+	if (stn.state == FLOATLBS)
+	{
+		std::cout << stn.pounds << " pounds.\n";
+	}
+	else if (stn.state == INTLBS)
+	{
+		std::cout << static_cast<int>(stn.pounds) << " integer pounds.\n";
+	}
+	else if (stn.state == STONE)
+	{
+		std::cout << stn.stone << " stones, " << stn.pds_left << " pounds\n";
+	}
+	return os;
+}
+
+bool Stonewt::operator==(const Stonewt & b) const
+{
+	if (pounds == b.pounds)
+		return true;
+	else
+		return false;
+}
+
+bool Stonewt::operator!=(const Stonewt & b) const
+{
+	if (pounds != b.pounds)
+		return true;
+	else
+		return false;
+}
+bool Stonewt::operator>(const Stonewt & b) const
+{
+	if (pounds > b.pounds)
+		return true;
+	else
+		return false;
+}
+bool Stonewt::operator<(const Stonewt & b) const
+{
+	if (pounds < b.pounds)
+		return true;
+	else
+		return false;
+}
+bool Stonewt::operator<=(const Stonewt & b) const
+{
+	if (pounds <= b.pounds)
+		return true;
+	else
+		return false;
+}
+bool Stonewt::operator>=(const Stonewt & b) const
+{
+	if (pounds >= b.pounds)
+		return true;
+	else
+		return false;
+}
